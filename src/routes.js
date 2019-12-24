5f2cbdc68b0427593abc2daf9c9aa7124a823cee
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-24 00:45:21 -0600

    header component added

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Layout from './Hoc/Layout';
 
 const Routes = (props) => {
   return(
-    <div>
-      hello
-    </div>
+    <Layout>
+      second line
+    </Layout>
   )
 }
 
