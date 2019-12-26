088ed26a1ece9165c9be217634f3312b68cbcfe3
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-25 22:59:45 -0600

    ADDS Firebase Snapshot and data recieved

--- src/firebase.js
+++ src/firebase.js
@@ -14,3 +14,9 @@ const config = {
   };
 
   firebase.initializeApp(config);
+
+  const firebaseDB = firebase.database();
+
+  firebaseDB.ref('matches').once('value').then((snapshot)=>{
+        console.log(snapshot.val())
+  })
