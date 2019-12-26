f35b980b01515d062de714f72ad685fd7c1b4bbc
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-26 01:05:02 -0600

    adds 5of6 matches showing

--- src/Components/home/matches/Blocks.js
+++ src/Components/home/matches/Blocks.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { firebaseMatches } from '../../../firebase';
+import { firebaseLooper, reverseArray } from '../../ui/misc';
+
+import MatchesBlock from '../../ui/matches_block';
+import Slide from 'react-reveal/Slide'
 
 class Blocks extends Component {
 state = {
@@ -8,19 +12,32 @@ state = {
 
 componentDidMount() {
     firebaseMatches.limitToLast(6).once('value').then((snapshot)=>{
-        console.log(snapshot.val())
+        const matches = firebaseLooper(snapshot);
+
+        this.setState({
+            matches: reverseArray(matches)
+        });
     })
 }
 
-showMatches = () => (
-    <div>
-        match
-    </div>
+showMatches = (matches) => (
+    matches ?
+            matches.map((match)=>(
+                <Slide bottom key={match.id}>
+                    <div className="item">
+                        <div className="wrapper">
+                            <MatchesBlock match={match}/>
+                        </div>
+                    </div>
+                </Slide>
+            ))
+        :null
 
 )
 
 
     render() {
+        console.log(this.state)
         return (
             <div className="home_matches">
                {this.showMatches(this.state.matches)} 
