4e3dd15da85dc45c6f6f1820166242520e102dc0
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-25 21:47:32 -0600

    adds LINK TO MORE MACTHES

--- src/Components/home/matches/index.js
+++ src/Components/home/matches/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Tag } from '../../ui/misc';
 
 const MatchesHome = () => {
     return (
         <div className="home_matches_wrapper">
             <div className="container">
-                TAG
-
+                <Tag
+                bck="#0e1731"
+                size="50px"
+                color="#ffffff"
+                add={{
+                    color: 'red'
+                }}
+                >
+                Matches
+                </Tag>
                 BLOCK
+                <Tag
+                bck="#ffffff"
+                size="22px"
+                color="#0e1731"
+                link={true}
+                linkto="/theteam"
+                >
+                    See more matches
+                </Tag>
 
                 TAG
             </div>
