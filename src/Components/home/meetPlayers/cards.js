164774f10fe1358dce76ce1fa86718abdffae642
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-29 16:13:43 -0600

    adds MEET PLAYERS

--- src/Components/home/meetPlayers/cards.js
+++ src/Components/home/meetPlayers/cards.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
 import Otamendi from '../../../Resources/images/players/Otamendi.png';
+import PlayerCard from '../../ui/playerCard';
+
 class HomeCards extends Component {
 
     state = {
@@ -47,7 +49,12 @@ showAnimateCards = () => (
                           bottom 
                     }}
                     >
-                        div
+                        <PlayerCard
+                        number="30"
+                        name="Nicolas"
+                        lastname="Otamendi"
+                        bck={Otamendi}
+                        />
                     </div>
                 )
                 
