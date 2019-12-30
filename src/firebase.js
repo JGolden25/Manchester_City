6b03f72f1e9dffa3a53b11bb30539639f35fee82
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-30 15:12:52 -0600

    RELOADED SERVER login works and sends user to dashboard

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/app'
 import 'firebase/app'
 import 'firebase/database'
+import 'firebase/auth'
 
 const config = {
     apiKey: "AIzaSyA9WVEKJshbz055gcW9rVkogHvrsdJZQu4",
