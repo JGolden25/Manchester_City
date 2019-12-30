3dbfde6331ab79d295988b882fdb0c4362497077
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-30 15:44:26 -0600

    addds admin layout and dashboard files

---
+++ src/Components/admin/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Dashboard = () => {
+
+return (
+<div>
+    <div>
+        this is dashboard.
+    </div>
+</div>
+
+)
+
+
+}
+
+export default Dashboard;
---
+++ src/Hoc/AdminLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminLayout = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AdminLayout;
