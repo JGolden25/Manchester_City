f36669c46aa638ddfd83a13b551e4484b326baca
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-30 19:03:17 -0600

    adds ADMIN MATCHES with material ui tables

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,13 @@ import PublicRoute from './Components/authRoutes/publicRoutes';
 import Home from './Components/home';
 import SignIn from './Components/signin';
 import Dashboard from './Components/admin/Dashboard';
+import AdminMatches from './Components/admin/matches';
 
 const Routes = (props) => {
   return(
     <Layout>
       <Switch>
+      <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches}/>
       <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
      <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn}/>
       <PublicRoute {...props} restricted={false} path="/" exact component={Home}/>
