bf419d6b8045a44098b6270e0487707e25b7b8ed
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-01 17:54:44 -0600

    add players route and component created

--- src/routes.js
+++ src/routes.js
@@ -9,17 +9,20 @@ import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
 import AddEditMatch from './Components/admin/matches/addEditMatch';
 import AdminPlayers from './Components/admin/players';
+import AddEditPlayers from './Components/admin/players/addEditPlayers';
 
 const Routes = (props) => {
   return(
     <Layout>
       <Switch>
       <PrivateRoute {...props} path="/admin_players" exact component={AdminPlayers}/>
+      <PrivateRoute {...props} path="/admin_matches/add_players" exact component={AddEditPlayers}/>
+      <PrivateRoute {...props} path="/admin_matches/add_players/:id" exact component={AddEditPlayers}/>
       <PrivateRoute {...props} path="/admin_matches/edit_match" exact component={AddEditMatch}/>
       <PrivateRoute {...props} path="/admin_matches/edit_match/:id" exact component={AddEditMatch}/>
       <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches}/>
       <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
-     <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn}/>
+      <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn}/>
       <PublicRoute {...props} restricted={false} path="/" exact component={Home}/>
       </Switch>
     </Layout>
