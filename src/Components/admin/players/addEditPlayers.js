ff3e24c75417e6dce3abded82110b84b5710d1c5
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-01 18:24:34 -0600

    adds state of form data for ADD PLAYERS

--- src/Components/admin/players/addEditPlayers.js
+++ src/Components/admin/players/addEditPlayers.js
@@ -1,8 +1,98 @@
 import React, { Component } from 'react';
+import AdminLayout from '../../../Hoc/AdminLayout';
+
+import FormField from '../../ui/formFields';
+import { validate } from '../../ui/misc';
+
+import Fileuploader from '../../ui/fileuploader';
+import { firebasePlayers , firebaseDB, firebase } from '../../../firebase';
 
 class AddEditPlayers extends Component {
 
-    
+    state = {
+        playerId:'',
+        formType:'',
+        formError: false,
+        formSuccess:'',
+        defaultImg:'',
+        formdata:{
+            name:{
+                element:'input',
+                value:'',
+                config:{
+                    label: 'Player Name',
+                    name:'name_input',
+                    type: 'text'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            lastname:{
+                element:'input',
+                value:'',
+                config:{
+                    label: 'Player Last name',
+                    name:'lastname_input',
+                    type: 'text'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            number:{
+                element:'input',
+                value:'',
+                config:{
+                    label: 'Player number',
+                    name:'number_input',
+                    type: 'text'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            position:{
+                element:'select',
+                value:'',
+                config:{
+                    label: 'Select a position',
+                    name:'select_position',
+                    type: 'select',
+                    options: [
+                        {key:"Keeper",value:"Keeper"},
+                        {key:"Defence",value:"Defence"},
+                        {key:"Midfield",value:"Midfield"},
+                        {key:"Striker",value:"Striker"}
+                    ]
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            image:{
+                element:'image',
+                value:'',
+                validation:{
+                    required: true
+                },
+                valid:false
+            }
+        }
+    }
+
 
 
     render() {
