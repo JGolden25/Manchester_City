c416ba3422be9d7bab681c419a2ec7a476e36d37
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-02 17:16:38 -0600

    adds routes for the team

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import PrivateRoute from './Components/authRoutes/privateRoutes';
 import PublicRoute from './Components/authRoutes/publicRoutes';
 import Home from './Components/home';
 import SignIn from './Components/signin';
+import TheTeam from './Components/theTeam';
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
 import AddEditMatch from './Components/admin/matches/addEditMatch';
@@ -23,6 +24,7 @@ const Routes = (props) => {
       <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches}/>
       <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
       <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn}/>
+      <PublicRoute {...props} restricted={false} path="/the_team" exact component={TheTeam}/>
       <PublicRoute {...props} restricted={false} path="/" exact component={Home}/>
       </Switch>
     </Layout>
