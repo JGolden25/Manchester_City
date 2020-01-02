6280aad90e43af5070f6b60ff8dea6de15e96b97
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-01 18:33:12 -0600

    adds filestore and formfields for players

--- src/Components/admin/players/addEditPlayers.js
+++ src/Components/admin/players/addEditPlayers.js
@@ -93,6 +93,51 @@ class AddEditPlayers extends Component {
         }
     }
 
+    updateFields = (player, playerId, formType , defaultImg) =>{
+        const newFormdata = { ...this.state.formdata}
+
+        for(let key in newFormdata){
+            newFormdata[key].value = player[key];
+            newFormdata[key].valid = true
+        }
+
+        this.setState({
+            playerId,
+            defaultImg,
+            formType,
+            formdata: newFormdata
+        })
+    }
+
+
+    componentDidMount(){
+        const playerId = this.props.match.params.id;
+
+        if(!playerId){
+            this.setState({
+                formType:'Add player'
+            })
+        } else {
+           firebaseDB.ref(`players/${playerId}`).once('value')
+           .then(snapshot => {
+               const playerData = snapshot.val();
+
+                firebase.storage().ref('players')
+                .child(playerData.image).getDownloadURL()
+                .then( url => {
+                    this.updateFields(playerData,playerId,'Edit player',url)
+                }).catch( e => {
+                    this.updateFields({
+                        ...playerData,
+                        image:''
+                    },playerId,'Edit player','')
+                })
+           })
+        }
+
+    }
+
+
     updateForm(element, content = ''){
         const newFormdata = {...this.state.formdata}
         const newElement = { ...newFormdata[element.id]}
@@ -115,13 +160,133 @@ class AddEditPlayers extends Component {
         })
     }
 
+    successForm = (message) => {
+        this.setState({
+            formSuccess: message
+        });
+        setTimeout(()=>{
+            this.setState({
+                formSuccess:''
+            });
+        },2000)
+
+    }
+
+    submitForm(event){
+        event.preventDefault();
+        
+        let dataToSubmit = {};
+        let formIsValid = true;
+
+        for(let key in this.state.formdata){
+            dataToSubmit[key] = this.state.formdata[key].value;
+            formIsValid = this.state.formdata[key].valid && formIsValid;
+        }
+    
+        if(formIsValid){
+            if(this.state.formType === 'Edit player'){
+                firebaseDB.ref(`players/${this.state.playerId}`)
+                .update(dataToSubmit).then(()=>{
+                    this.successForm('Update correctly');
+                }).catch(e=>{
+                    this.setState({formError: true})
+                })
+            } else {
+                firebasePlayers.push(dataToSubmit).then(()=>{
+                    this.props.history.push('/admin_players')
+                }).catch(e=>{
+                    this.setState({
+                        formError: true
+                    })
+                })
+            }
+           
+        } else {
+            this.setState({
+                formError: true
+            })
+        }
+    }
+
+    resetImage = () => {
+        const newFormdata = {...this.state.formdata}
+        newFormdata['image'].value = '';
+        newFormdata['image'].valid = false;
+        
+        this.setState({
+            defaultImg:'',
+            formdata: newFormdata
+        })
+    }
+
+    storeFilename = (filename) => {
+        this.updateForm({id:'image'},filename)
+    }
+
 
 
     render() {
         return (
-            <div>
-                
-            </div>
+            <AdminLayout>
+                <div className="editplayers_dialog_wrapper">
+                    <h2>
+                        {this.state.formType}
+                    </h2>
+                    <div>
+                        <form onSubmit={(event)=> this.submitForm(event)}>
+            
+                            <Fileuploader
+                                dir="players"
+                                tag={"Player image"}
+                                defaultImg={this.state.defaultImg}
+                                defaultImgName={this.state.formdata.image.value}
+                                resetImage={()=> this.resetImage()}
+                                filename={(filename)=> this.storeFilename(filename)}
+                            />
+
+
+                            <FormField
+                                id={'name'}
+                                formdata={this.state.formdata.name}
+                                change={(element)=> this.updateForm(element)}
+                                
+                            />
+
+                            <FormField
+                                id={'lastname'}
+                                formdata={this.state.formdata.lastname}
+                                change={(element)=> this.updateForm(element)}
+                            />
+
+                            <FormField
+                                id={'number'}
+                                formdata={this.state.formdata.number}
+                                change={(element)=> this.updateForm(element)}
+                            />
+
+                            <FormField
+                                id={'position'}
+                                formdata={this.state.formdata.position}
+                                change={(element)=> this.updateForm(element)}
+                            />
+
+                        <div className="success_label">{this.state.formSuccess}</div>
+                            {this.state.formError ? 
+                                <div className="error_label">
+                                    Something is wrong
+                                </div>
+                                : ''
+                            }
+                            <div className="admin_submit">
+                                <button onClick={(event)=> this.submitForm(event)}>
+                                    {this.state.formType}
+                                </button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </AdminLayout>
         );
     }
 }
